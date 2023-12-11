fde53f4f28a3290f589a51b274cab056336e5286
Daniel Teel <daniel.teel.2@us.af.mil>
2023-12-11 15:55:23 -0700

    asd

--- index.js
+++ index.js
@@ -1,53 +1,89 @@
 
-const {exec, execSync} = require('node:child_process');
+const { exec, execSync } = require('node:child_process');
 const process = require('node:process');
 const download = require('github-directory-downloader');
-const { mkdirSync, cpSync, rmSync }=require('node:fs');
+const { mkdirSync, cpSync, rmSync } = require('node:fs');
 const path = require('node:path');
 const chalk = require('chalk');
-
+const app = require('express')();
+const port = 4002;
 require('dotenv').config({ path: 'env.env' });
 
-let backendProcess=null;
+let backendProcess = null;
 
-let status={
-    front:{
+let status = {
+    webhook: {
+        active: false,
+        messages: [],
+        maxMessages: 3
+    },
+    front: {
         dir: false,
         downloaded: false
     },
-    back:{
+    back: {
         dir: false,
         downloaded: false,
         installed: false,
-        messages:[],
-        running: false
+        messages: [],
+        running: false,
+        maxMessages: 10
     }
 };
 
-function updateScreen(which, key, val){
-    if (which==='back' && key==='messages'){
-        status.back.messages.push(val);
-        if (status.back.messages.length>5){
-            status.back.messages.shift();
-        }
+app.post('/backend/:secret', (req, res) => {
+    if (req.params.secret === process.env.GITPUSH_SECRET) {
+        doBackend();
+        updateScreen('webhook', 'messages', chalk.green('BACKEND PUSH PASSED'));
     }else{
-        status[which][key]=val;
+        updateScreen('webhook', 'messages', chalk.red('BACKEND PUSH BADSECRET'));
+    }
+    res.sendStatus(200);
+})
+app.post('/frontend/:secret', (req, res) => {
+    if (req.params.secret === process.env.GITPUSH_SECRET) {
+        doFrontend();
+        updateScreen('webhook', 'messages', chalk.green('FRONTEND PUSH PASSED'));
+    }else{
+        updateScreen('webhook', 'messages', chalk.red('FRONTEND PUSH BADSECRET'));
+    }
+    res.sendStatus(200);
+})
+app.listen(port, () => {
+    updateScreen('webhook', 'active', true);
+})
+
+function updateScreen(which, key, val) {
+    if (key === 'messages') {
+        status[which].messages.push(val);
+        if (status[which].messages.length > status[which].maxMessages) {
+            status[which].messages.shift();
+        }
+    } else {
+        status[which][key] = val;
     }
     console.clear();
     console.log(chalk.yellow('HSEC-CONTROLLER'));
     console.log();
+    console.log(chalk.cyan('WEBHOOK'));
+    console.log(status.webhook.active ? chalk.green('X') : chalk.yellow('.'), chalk.white('ACTIVE'));
+    console.log(chalk.yellow('MESSAGES'));
+    for (let m of status.webhook.messages) {
+        console.log('\t', m.trim());
+    }
+    console.log();
     console.log(chalk.cyan('FRONT'));
-    console.log(status.front.dir?chalk.green('X'):chalk.yellow('.'),chalk.white('DIRECTORY'));
-    console.log(status.front.downloaded?chalk.green('X'):chalk.yellow('.'),chalk.white('DOWNLOADED'));
+    console.log(status.front.dir ? chalk.green('X') : chalk.yellow('.'), chalk.white('DIRECTORY'));
+    console.log(status.front.downloaded ? chalk.green('X') : chalk.yellow('.'), chalk.white('DOWNLOADED'));
     console.log();
 
     console.log(chalk.cyan('BACK'));
-    console.log(status.back.dir?chalk.green('X'):chalk.yellow('.'),chalk.white('DIRECTORY'));
-    console.log(status.back.downloaded?chalk.green('X'):chalk.yellow('.'),chalk.white('DOWNLOADED'));
-    console.log(status.back.installed?chalk.green('X'):chalk.yellow('.'),chalk.white('INSTALLED'));
-    console.log(status.back.running?chalk.green('X'):chalk.yellow('.'),chalk.white('RUNNING'));
+    console.log(status.back.dir ? chalk.green('X') : chalk.yellow('.'), chalk.white('DIRECTORY'));
+    console.log(status.back.downloaded ? chalk.green('X') : chalk.yellow('.'), chalk.white('DOWNLOADED'));
+    console.log(status.back.installed ? chalk.green('X') : chalk.yellow('.'), chalk.white('INSTALLED'));
+    console.log(status.back.running ? chalk.green('X') : chalk.yellow('.'), chalk.white('RUNNING'));
     console.log(chalk.yellow('MESSAGES'));
-    for (let m of status.back.messages){
+    for (let m of status.back.messages) {
         console.log('\t', m.trim());
     }
 }
@@ -57,11 +93,13 @@ doBackend();
 
 
 //Get front end
-function doFrontend(){
+function doFrontend() {
+    updateScreen('front', 'downloaded', false);
+
     try {
         mkdirSync('./front');
-    }catch (e){
-        if (e.code!=='EEXIST'){
+    } catch (e) {
+        if (e.code !== 'EEXIST') {
             console.log(e);
             process.exit(-1);
         }
@@ -69,15 +107,15 @@ function doFrontend(){
 
     try {
         rmSync('/mnt/ramdisk/static', { recursive: true, force: true });
-    }catch (e){
+    } catch (e) {
         console.log(e);
         process.exit(-1);
     }
 
     try {
         mkdirSync('/mnt/ramdisk/static');
-    }catch (e){
-        if (e.code!=='EEXIST'){
+    } catch (e) {
+        if (e.code !== 'EEXIST') {
             console.log(e);
             process.exit(-1);
         }
@@ -86,62 +124,66 @@ function doFrontend(){
 
     updateScreen('front', 'dir', true);
 
-    download('https://github.com/danielteel/hsec/tree/main/build', './front', {requests: 1, muteLog: true}).then( (stats) => {
+    download('https://github.com/danielteel/hsec/tree/main/build', './front', { requests: 1, muteLog: true }).then((stats) => {
         try {
-            cpSync('./front/', '/mnt/ramdisk/static/', {recursive: true});
-        }catch (e){
+            cpSync('./front/', '/mnt/ramdisk/static/', { recursive: true });
+        } catch (e) {
             console.log(e);
             process.exit(-1);
         }
         updateScreen('front', 'downloaded', true);
-    }).catch(e=>{
+    }).catch(e => {
         console.log(e);
         process.exit(-1);
     })
-    
+
 }
 
 
 //Get back end
-function doBackend(){
-    if (backendProcess){
+function doBackend() {
+    status.back.installed = false;
+    updateScreen('back', 'downloaded', false);
+
+    if (backendProcess) {
         backendProcess.kill();
+
     }
     try {
         mkdirSync('./back');
-    }catch (e){
-        if (e.code!=='EEXIST'){
+    } catch (e) {
+        if (e.code !== 'EEXIST') {
             console.log(e);
             process.exit(-1);
         }
     }
-    
+
     updateScreen('back', 'dir', true);
-    try{
-        execSync('git clone https://github.com/danielteel/hsec-api', {cwd: path.join(__dirname, 'back') });
-    }catch{}
-    try{
-        execSync('git pull', {cwd: path.join(__dirname, 'back', 'hsec-api') });
-    }catch{}
-    
+    try {
+        execSync('git clone https://github.com/danielteel/hsec-api', { cwd: path.join(__dirname, 'back') });
+    } catch { }
+    try {
+        execSync('git pull', { cwd: path.join(__dirname, 'back', 'hsec-api') });
+    } catch { }
+
     updateScreen('back', 'downloaded', true);
     try {
-        execSync('npm install', {cwd: path.join(__dirname, 'back', 'hsec-api') });
-    }catch{}
+        execSync('npm install', { cwd: path.join(__dirname, 'back', 'hsec-api') });
+    } catch { }
     updateScreen('back', 'installed', true);
 
-    backendProcess = exec('node index', {cwd: path.join(__dirname, 'back', 'hsec-api'), env: {...process.env}});
+    backendProcess = exec('node index', { cwd: path.join(__dirname, 'back', 'hsec-api'), env: { ...process.env } });
 
     updateScreen('back', 'running', true);
 
-    backendProcess.on('exit', (code)=>{
+    backendProcess.on('exit', (code) => {
         updateScreen('back', 'running', false);
-        backendProcess=null;
+        backendProcess = null;
     });
-    backendProcess.stderr.on('data', (d)=>{
+    backendProcess.stderr.on('data', (d) => {
         updateScreen('back', 'messages', d);
     });
-    backendProcess.stdout.on('data', (d)=>{
+    backendProcess.stdout.on('data', (d) => {
         updateScreen('back', 'messages', d);
     });
 }

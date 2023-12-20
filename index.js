624e5db5972488d9ef4e097e65a4ac6caa8acf73
Daniel Teel <daniel.teel.2@us.af.mil>
2023-12-19 23:49:10 -0700

    asd

--- index.js
+++ index.js
@@ -218,9 +218,9 @@ function doCamProcess(){
     updateScreen('ffmpeg','dir', true);
     
     const formats = [
-        {file: 'pqll.m3u8', title:'Low',  w: 320,  h: 180, qual: 23, fps: 4, block: 2},//11 kbps
-        {file: 'hqll.m3u8', title:'Med', w: 640, h: 360, qual: 23, fps: 4, block: 2},//225 kbps
-        {file: 'best.m3u8', title:'High', w: 1280, h: 720, qual: 23, fps: 4, block: 2},//350 kbps
+        {file: 'pqll.m3u8', title:'Low',  w: 320,  h: 180, qual: 21, fps: 4, block: 2},//11 kbps
+        {file: 'hqll.m3u8', title:'Med', w: 640, h: 360, qual: 23, fps: 4, block: 2},//36 kbps
+        {file: 'best.m3u8', title:'High', w: 1280, h: 720, qual: 25, fps: 4, block: 2},//250 kbps
     ];
     writeFileSync('/mnt/ramdisk/cam/details.json', JSON.stringify(formats));
 
